41e61a7c2749a72a16ff78b278eae73c4ffc8e7e
caseBread <kgu0515@gmail.com>
2023-01-26 15:29:44 +0900

    style : tailwind css의 width 정의 추가 및 html의 기본 css 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i * 0.25}rem`) };
+
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
+  safelist: [{ pattern: /w-./ }],
   theme: {
-    extend: {},
+    extend: {
+      width: px0_200,
+    },
   },
   plugins: [],
 };
